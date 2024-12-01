de1ffb94ed38fc6942fc8575c734154f234897fd
Sofia Tovedal <sofia.tovedal@gmail.com>
2024-12-01 13:31:20 +0100

    feat: util for parsing and validating user arguments solution identifier and session token

--- utils/userinput.go
+++ utils/userinput.go
@@ -7,13 +7,13 @@ import (
 
 func ParseUserArguments(existingSolutions int) (int, string) {
 	if len(os.Args) < 3 {
-		panic("Arguments missing, provide solutionIdentifier to solve and session token")
+		panic("Arguments missing, provide solution identifier and session token")
 	}
 
 	solutionIdentifier, err := strconv.Atoi(os.Args[1])
 
 	if err != nil || solutionIdentifier < 1 || solutionIdentifier > existingSolutions {
-		panic("Provided solutionIdentifier argument is invalid: %v, solutionIdentifiers up to %v exists\n")
+		panic("Provided solution identifier argument is out of range")
 	}
 
 	sessionToken := os.Args[2]
